efa04fb4fa7f9cc9e7073ae7999ba197a46a450d
kbbushman <k.bushman@yahoo.com>
2016-11-19 22:38:31 -0800

    adds user update controller action and view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
+
 	has_many :children
 	has_many :galleries
 	has_many :photos
 
 	has_secure_password
 
-  validates :first_name, :last_name, :email, :password, presence: true, :on => :create
+  validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
-  validates :password, length: { minimum: 6 }, :on => :create
+  validates :password, presence: true, length: { minimum: 6 }, :on => :create
 
 end

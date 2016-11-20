8679353c0e66d545108166c11a3093e7b1ab752e
kbbushman <k.bushman@yahoo.com>
2016-11-20 00:31:13 -0800

    adds log in, log out, and current user methods

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+
+	def login(user)
+	  session[:user_id] = user.id
+	  @current_user = user
+	end
+
+	def current_musician
+	  @current_user||= User.find_by_id(session[:user_id])
+	end
+
+	def logout
+	  @current_user = session[:user_id] = nil
+	  flash[:success] = "Successfully logged out."
+	  redirect_to root_path
+	end
+
 end

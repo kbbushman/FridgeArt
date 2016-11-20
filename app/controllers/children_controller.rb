e8cdef9d5cbfc91d563776682947d90045da7204
kbbushman <k.bushman@yahoo.com>
2016-11-20 00:21:59 -0800

    adds create child method

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,2 +1,32 @@
 class ChildrenController < ApplicationController
+
+	def index
+		@children = Child.all
+	end
+
+	def new
+		@child = Child.new
+	end
+
+	def create
+		@child = Child.create(create_child_params)
+		if @child.save
+		  flash[:success] = 'Your Child Has Been Added.'
+		  redirect_to @user
+		else
+		  flash[:error] = @child.errors.full_messages.join('. ')
+		  render :new
+		end
+	end
+
+	def show
+	end
+
+
+	private
+
+	def create_child_params
+		params.require(:child).permit(:child_name)
+	end
+
 end

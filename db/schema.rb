dd499100fa184e22eabec93199d8b6bb81e0229b
kbbushman <k.bushman@yahoo.com>
2016-11-19 17:12:24 -0800

    creates gallery schema and adds association to child model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161120003958) do
+ActiveRecord::Schema.define(version: 20161120010807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 20161120003958) do
     t.index ["user_id"], name: "index_children_on_user_id", using: :btree
   end
 
+  create_table "galleries", force: :cascade do |t|
+    t.string   "gallery_name"
+    t.string   "gallery_image"
+    t.integer  "child_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["child_id"], name: "index_galleries_on_child_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
@@ -34,4 +43,5 @@ ActiveRecord::Schema.define(version: 20161120003958) do
   end
 
   add_foreign_key "children", "users"
+  add_foreign_key "galleries", "children"
 end

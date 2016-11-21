ee4e9625658c28d29b05bf30abd6516663d5f1a5
kbbushman <k.bushman@yahoo.com>
2016-11-21 09:50:52 -0800

    adds create photo methods

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,27 @@
 class PhotosController < ApplicationController
+
+	def index
+		@photos = Photo.all
+	end
+
+	def new
+		@photo = Photo.new
+	end
+
+	def create
+		@photo = current_user
+						.photos
+						.new(create_photo_params)
+
+		@photo.save
+
+		if @photo.save
+		  flash[:success] = 'Your Photo Has Been Added.'
+		  redirect_to current_user
+		else
+		  flash[:error] = @photo.errors.full_messages.join('. ')
+		  render :new
+		end
+	end
+
 end

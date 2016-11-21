a6930eda812e95db6533daa5c45783d88e4b0967
kbbushman <k.bushman@yahoo.com>
2016-11-20 23:51:25 -0800

    adds gallery create methods

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,2 +1,38 @@
 class GalleriesController < ApplicationController
+
+	def index
+		@galleries = Gallery.all
+	end
+
+	def new
+		@gallery = Gallery.new
+	end
+
+	def create
+		@gallery = current_user
+			.galleries
+			.new(create_gallery_params)
+
+		@gallery.save
+
+		if @gallery.save
+		  flash[:success] = 'Your Gallery Has Been Created.'
+		  redirect_to current_user
+		else
+		  flash[:error] = @gallery.errors.full_messages.join('. ')
+		  render :new
+		end
+	end
+
+	def show
+	end
+
+
+	private
+
+	def create_gallery_params
+		params.require(:gallery).permit(:gallery_name, :child_id)
+	end
+
+
 end

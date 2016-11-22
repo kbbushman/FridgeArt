2ab6a3d1efcf4231158f2e40538cd3e6364aefd3
kbbushman <k.bushman@yahoo.com>
2016-11-22 09:49:44 -0800

    adds generate custom slug method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,22 @@
 class User < ApplicationRecord
 
-	has_many :children
-	has_many :galleries
-	has_many :photos
+	has_many :children, dependent: :destroy
+	has_many :galleries, dependent: :destroy
+	has_many :photos, dependent: :destroy
 
 	has_secure_password
 
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }, :on => :create
+
+  extend FriendlyId
+  friendly_id :generate_custom_slug, use: :slugged
+
+
+  def generate_custom_slug
+   "#{first_name}_#{last_name}"
+  end
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
